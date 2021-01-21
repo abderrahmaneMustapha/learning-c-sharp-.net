ae62f4d1269d97a573f3dcc71ca5f5d3b2da9953
abderrahmane <abderrahmaneMustapha030898@gmail.com>
2021-01-21 18:34:32 +0100

    feat :  build and debug

--- main/projects/gradebook/src/GradeBook/Program.cs
+++ main/projects/gradebook/src/GradeBook/Program.cs
@@ -6,7 +6,12 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if( args.Length > 0){
+                Console.WriteLine($"Hello {args[0]} !");
+            }
+            else {
+                Console.WriteLine("Hello  !");
+            }
         }
     }
 }
