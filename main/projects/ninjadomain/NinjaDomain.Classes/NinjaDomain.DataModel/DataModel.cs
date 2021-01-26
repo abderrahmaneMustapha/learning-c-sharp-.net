c2463ce9d5aa01db45eae1501715e846d890e75e
abderrahmane <abderrahmaneMustapha030898@gmail.com>
2021-01-27 00:10:42 +0100

    an on model creating method assign modefied date and createed date

--- main/projects/ninjadomain/NinjaDomain.Classes/NinjaDomain.DataModel/DataModel.cs
+++ main/projects/ninjadomain/NinjaDomain.Classes/NinjaDomain.DataModel/DataModel.cs
@@ -1,5 +1,7 @@
-﻿using System.Data.Entity;
-using NinjaDomain.Classes;
+﻿using NinjaDomain.Classes;
+using System;
+using System.Data.Entity;
+using System.Linq;
 
 namespace NinjaDomain.DataModel
 {
@@ -8,5 +10,34 @@ namespace NinjaDomain.DataModel
         public DbSet<Ninja> ninjas { get; set; }
         public DbSet<Clan> clans { get; set; }
         public DbSet<NinjaEquipement> equipements { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Types().Configure(c => c.Ignore("IsDirty"));
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public override int SaveChanges()
+        {
+            foreach (var history in this.ChangeTracker.Entries()
+                .Where(e => e.Entity is IModificationHistory &&
+                (e.State == EntityState.Added || e.State == EntityState.Modified
+            )).Select(e => e.Entity as IModificationHistory)
+                )
+            {
+                history.DateModified = DateTime.Now;
+                if (history.DateCreated == DateTime.MinValue)
+                {
+                    history.DateCreated = DateTime.Now;
+                }
+            }
+            int result = base.SaveChanges();
+            foreach (var history in this.ChangeTracker.Entries()
+                .Where(e => e.Entity is IModificationHistory).Select(e => e.Entity as IModificationHistory))
+            {
+                history.IsDirty = false;
+            }
+            return result;
+        }
     }
 }
